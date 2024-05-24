8ee0921dc336da7fdc2e553e5eda80c2b9231529
user.name: gabrielhaguero@gmail.com <gabrielhaguero@gmail.com>
2024-05-24 01:48:04 -0300

    actualizo section main

--- script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementById("menu");
-const navItems = document.getElementById("nav-items");
-
-menu.addEventListener("click", (e) => {
-  e.preventDefault();
-  navItems.className += "menu-responsive";
-});
