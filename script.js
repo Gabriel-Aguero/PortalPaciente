27740ea1bbc4d05b14246fbd36875bb178cba0f5
user.name: gabrielhaguero@gmail.com <gabrielhaguero@gmail.com>
2024-05-25 16:43:40 -0300

    subiendo pages datos clinicos

--- script.js
+++
@@ -1,8 +0,0 @@
-const files = document.getElementById("foto_field");
-
-files.addEventListener("change", () => {
-  const filename = this.files[0]
-    ? this.files[0].name
-    : "Ningun archivo seleccionado";
-  document.getElementById("file-name").textContent = filename;
-});
