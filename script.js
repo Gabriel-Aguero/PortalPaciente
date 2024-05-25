86daecbc3ce472d6c9e23621e40fe75232e3a619
user.name: gabrielhaguero@gmail.com <gabrielhaguero@gmail.com>
2024-05-25 18:17:34 -0300

    agrego menu responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menu = document.getElementById("menu");
+  const navItems = document.getElementById("navbar");
+
+  menu.addEventListener("click", function () {
+    navItems.classList.toggle("active");
+  });
+});
