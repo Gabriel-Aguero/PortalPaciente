6bb18c9b5885e4ce009ef2286a62183ea2a5386b
user.name: gabrielhaguero@gmail.com <gabrielhaguero@gmail.com>
2024-05-25 11:58:09 -0300

    subiendo actulizaciones para post merge

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const menu = document.getElementById("menu");
-const navItems = document.getElementById("nav-items");
 
-menu.addEventListener("click", (e) => {
-  e.preventDefault();
-  navItems.className += "menu-responsive";
-});
